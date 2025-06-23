4e436b2efdb5f04187fb37d1f0c944c81a72d9bf
seb3773 <99963207+seb3773@users.noreply.github.com>
2025-06-23 15:09:35 +0200

    Update network.sh

--- assets/scripts/network.sh
+++ assets/scripts/network.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wid=$(xdotool search --onlyvisible --class "networkmanager")&&eval $(xdotool getwindowgeometry --shell $wid)&&xdotool mousemove $X $Y && xdotool click --window $wid 1
+wid=$(xdotool search --onlyvisible --class "networkmanager")&&eval $(xdotool getwindowgeometry --shell $wid)&&xdotool mousemove $((X+10)) $((Y+10)) && xdotool click --window $wid 1
